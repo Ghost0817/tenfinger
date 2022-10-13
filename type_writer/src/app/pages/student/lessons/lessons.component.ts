a3a473b57fbb69d5d3ef98ddb09b1bc1f3ffb8f4
Goodaway <sainzaya.b@gmail.com>
2022-10-13 17:13:04 +0800

    Update lessons.component.ts

--- type_writer/src/app/pages/student/lessons/lessons.component.ts
+++ type_writer/src/app/pages/student/lessons/lessons.component.ts
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonsComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated:boolean = false;
+  currentUser: any;
+  cateroties: any = [];
+  lessons: any = [];
+  curr_cate: any = [];
+  currLng = "MONGOLIAN";
+  wallOfFame: any;
+
+  constructor(private meta: Meta,private api: ApiService,private userService: UserService) {
+    this.meta.addTag({ name: 'description', content: 'An online application for typing fast with ten fingers.' });
+
+    this.api.get('/lesson').subscribe(response => {
+      this.curr_cate = response;
+      this.lessons = response;
+      this.cateroties = response.filter(function(data: any) {
+        return data.categoryParent == 17;
+      });
+    });
+
+    this.api.get('/wall-of-fame').subscribe(response => {
+      this.wallOfFame = response;
+    });
+
+    this.userService.isAuthenticated.pipe(distinctUntilChanged()).subscribe(isAuth =>{
+      this.isAuthenticated = isAuth
+    })
+    this.userService.currentUser.pipe(distinctUntilChanged()).subscribe(user => {
+      this.currentUser = user;
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  showContent() {
+
+  }
+
+  getLessons(category: any) {
+    console.log(category);
+    this.lessons = this.curr_cate.filter(function(data: any) {
+      return data.categoryParent == category;
+    });
+  }
+  
+  getCategory(langauge: string) {
+    this.currLng = langauge;
+
+    if(langauge=="MONGOLIAN") {
+      this.cateroties = this.curr_cate.filter(function(data: any) {
+        return data.categoryParent == 17;
+      });
+    }
+    if(langauge=="ENGLISH") {
+      this.cateroties = this.curr_cate.filter(function(data: any) {
+        return data.categoryParent == 18;
+      });
+    }
   }
 
 }
