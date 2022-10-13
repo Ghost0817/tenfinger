3a0b09edf4337c43e5a6d34bfcd509364558bc0c
Goodaway <sainzaya.b@gmail.com>
2022-10-13 17:03:41 +0800

    Update api.service.ts

--- type_writer/src/app/core/services/api.service.ts
+++ type_writer/src/app/core/services/api.service.ts
@@ -5,5 +5,39 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    // private jwtService: JwtService
+  ) {
+
+  }
+
+  private formatErrors(error: any) {
+    return  throwError(() => new Error(error.error));
+  }
+
+  get(path: string, params: HttpParams = new HttpParams(),): Observable<any> {
+    return this.http.get(`${environment.api_url}${path}`, { params })
+      .pipe(catchError(this.formatErrors));
+  }
+
+  put(path: string, body: Object = {}, http: any): Observable<any> {
+    return this.http.put(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body)
+    ).pipe(catchError(this.formatErrors));
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http.delete(
+      `${environment.api_url}${path}`
+    ).pipe(catchError(this.formatErrors));
+  }
 }
