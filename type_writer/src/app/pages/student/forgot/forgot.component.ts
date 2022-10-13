2f8e4cac571a1f7c151d7721e1a54365c3f4e58f
Goodaway <sainzaya.b@gmail.com>
2022-10-13 17:06:05 +0800

    Update forgot.component.ts

--- type_writer/src/app/pages/student/forgot/forgot.component.ts
+++ type_writer/src/app/pages/student/forgot/forgot.component.ts
@@ -7,9 +7,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotComponent implements OnInit {
 
-  constructor() { }
+  regForm!: FormGroup;
+  token: string|undefined;
+
+  showSuccessPage: boolean = false;
+
+  constructor(private http: HttpClient,private router: Router,private fb: FormBuilder) {
+    this.token = undefined;
+  }
 
   ngOnInit(): void {
+    this.regForm = new FormGroup({
+      email: new FormControl(
+        'Sainzaya@example.com',
+        {
+          validators: [
+            Validators.required,
+            Validators.email,
+            Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ]}),
+        recaptcha: new FormControl(
+          '',
+          {
+            validators: [
+              Validators.required,
+          ]})
+    });
+  }
+
+  get emailField () {
+    return this.regForm.get('email');
+  }
+  get recaptchaField () {
+    return this.regForm.get('recaptcha');
+  }
+
+  
+
+  onSubmit(event: any) {
+    if (this.regForm.invalid) {
+      for (const control of Object.keys(this.regForm.controls)) {
+        this.regForm.controls[control].markAsTouched();
+      }
+      return;
+    }
+
+    console.debug(`Token [${this.token}] generated`);
+    // TODO: Use EventEmitter with form value
+    const body = {
+      email: this.regForm.value['email'],
+    }
+    this.http.post(`${environment.api_url}/forgot`, body).subscribe(
+      (res: any) => {
+
+        this.showSuccessPage = true;
+      }
+    );
   }
 
 }
