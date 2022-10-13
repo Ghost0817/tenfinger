bfc2fc72e7faedab2baac9da99744b1992f4d5eb
Goodaway <sainzaya.b@gmail.com>
2022-10-13 17:04:20 +0800

    Update api.service.spec.ts

--- type_writer/src/app/core/services/api.service.spec.ts
+++ type_writer/src/app/core/services/api.service.spec.ts
@@ -6,11 +6,26 @@ describe('ApiService', () => {
   let service: ApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule],providers: [HttpClient]});
     service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should call get and return an data', fakeAsync(() => {
+    const resposeData =service.get('get').toPromise();
+    expect(resposeData).toMatch('Hello World');
+
+  }));
+
+  it('should call put and return an data', () => {
+  });
+
+  it('should call post and return an data', () => {
+  });
+
+  it('should call delete and return an data', () => {
   });
 });
