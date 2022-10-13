5c8fa25afccff052421bc9f3cae238ba49cabb8d
Goodaway <sainzaya.b@gmail.com>
2022-10-13 17:10:55 +0800

    Update login.component.ts

--- type_writer/src/app/pages/student/login/login.component.ts
+++ type_writer/src/app/pages/student/login/login.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+hide = true;
+  regForm!: FormGroup;
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private router: Router,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.regForm = new FormGroup({
+      username: new FormControl(
+      'sainaa',
+      {
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40)
+      ]}),
+      password: new FormControl(
+        'foo',
+        {
+          validators: [
+            Validators.required,
+            Validators.maxLength(120)
+        ]})
+    });
+  }
+
+  getError() {
+    return false;
+  }
+  
+  getGrade() {
+    return false;
+  }
+
+  login() {
+    const body = {
+      username: this.regForm.value['username'],
+      password: this.regForm.value['password']
+    }
+    this.http.post(`${environment.api_url}/authenticate`, body).subscribe((res: any) => {
+      localStorage.setItem('accessToken', res['jwt']);
+      this.userService.populate()
+      setTimeout(()=>{
+        this.router.navigateByUrl('/student/lesson');
+      }, 300)
+    })
   }
 
 }
