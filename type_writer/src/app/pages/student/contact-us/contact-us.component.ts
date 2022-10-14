8f2a8e95f142f05abeca1c65677556f812792ef3
Goodaway <sainzaya.b@gmail.com>
2022-10-14 16:22:03 +0800

    Update contact-us.component.ts

--- type_writer/src/app/pages/student/contact-us/contact-us.component.ts
+++ type_writer/src/app/pages/student/contact-us/contact-us.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated:boolean = false;
+  wallOfFame: any;
+
+  contactForm!: FormGroup;
+  token: string|undefined;
+
+  constructor(private http: HttpClient,private router: Router,private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('',{
+        validators: [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(40)
+        ]
+      }),
+      email: new FormControl(
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.email,
+            Validators.minLength(3),
+            Validators.maxLength(40),
+            Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+        ]}),
+        body: new FormControl('',{
+          validators: [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(500)
+          ]
+        }),
+        recaptcha: new FormControl(
+          '',
+          {
+            validators: [
+              Validators.required,
+          ]})
+    });
+  }
+
+  get recaptchaField () {
+    return this.contactForm.get('recaptcha');
+  }
+
+  onSubmit(event: any) {
+    if (this.contactForm.invalid) {
+      for (const control of Object.keys(this.contactForm.controls)) {
+        this.contactForm.controls[control].markAsTouched();
+      }
+      return;
+    }
+
+    const body = {
+      email: this.contactForm.value['email'],
+    }
+    this.http.post(`${environment.api_url}/contactus`, body).subscribe(
+      (res: any) => {
+
+      }
+    );
   }
 
 }
