49da141313616718df4df9bfd3d24daa2f113454
Goodaway <sainzaya.b@gmail.com>
2022-10-14 16:23:15 +0800

    Update upgrade.component.ts

--- type_writer/src/app/pages/student/upgrade/upgrade.component.ts
+++ type_writer/src/app/pages/student/upgrade/upgrade.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpgradeComponent implements OnInit {
 
+  upgradeData: any = [{
+    id: 'GOLD',
+    conditionTerm: '1',
+    price: '30,000',
+    featured: '1',
+    conditionOne: '16 Premium Lessons',
+    conditionTwo: 'Priority mail support',
+    conditionThree: '-'
+  },{
+    id: 'PLATINUM',
+    conditionTerm: '',
+    price: '50,000',
+    featured: '1',
+    conditionOne: '16 Premium Lessons',
+    conditionTwo: 'Priority mail support',
+    conditionThree: '-'
+  },{
+    id: 'SILVER',
+    conditionTerm: '1',
+    price: '14.99',
+    featured: '1',
+    conditionOne: '16 Premium Lessons',
+    conditionTwo: 'Priority mail support',
+    conditionThree: '-'
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getConditionTerm(obj: any): string{
+    let resultStr = '';
+    if(obj.conditionTerm) {
+      resultStr = obj.conditionTerm + ' year';
+    } else {
+      resultStr = 'lifetime';
+    }
+
+    return resultStr;
   }
 
 }
